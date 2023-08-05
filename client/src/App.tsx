e49dd7213e35bfab7c384b44de6fecbd51106fc1
hilee <hilee@iwillmed.com>
2023-08-05 17:38:57 +0900

    chore:초기 셋팅

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="border-solid border-2 border-red-400 h-full">
+        hello world
+      </div>
+    </>
+  );
+}
+
+export default App;
